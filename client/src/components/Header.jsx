4b454b0778e74358423e8a2cfa3fa4ae953632e3
Adisie Fenta <adisiefenta23@gmail.com>
2024-03-27 12:33:50 +0300

    some changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,17 +30,245 @@ const Header = () => {
     },
   ];
 
+  // criminal sub list
+  const civilSubNav = [
+    {
+      groupOne: [
+        {
+          navHeaderText: "Contract",
+          subNavList: [
+            {
+              navHeaderText: "Contract of special movables",
+              path: "#",
+            },
+            {
+              navHeaderText: "Sale or lease of buildings",
+              path: "#",
+            },
+            {
+              navHeaderText: "Construction",
+              path: "#",
+            },
+            {
+              navHeaderText: "Supply of goods and services",
+              path: "#",
+            },
+            {
+              navHeaderText: "Rental of machineries",
+              path: "#",
+            },
+            {
+              navHeaderText: "Loan",
+              path: "#",
+            },
+          ],
+        },
+        {
+          navHeaderText: "Family",
+          subNavList: [
+            {
+              navHeaderText: "Maintenance",
+              path: "#",
+            },
+            {
+              navHeaderText: "Adoption",
+              path: "#",
+            },
+            {
+              navHeaderText: "Divorce",
+              path: "#",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      groupTwo: [
+        {
+          navHeaderText: "Succession",
+          path: "#",
+        },
+        {
+          navHeaderText: "Employment",
+          path: "#",
+        },
+        {
+          navHeaderText: "Property",
+          path: "#",
+        },
+      ],
+    },
+  ];
+  // criminal sub list
+  const criminalSubNav = [
+    {
+      navHeaderText: 'Ordinary crime',
+      path: '#',
+    },
+    {
+      navHeaderText: 'Corruption',
+      path: '#',
+    },
+    {
+      navHeaderText: 'Money laundering',
+      path: '#',
+    },
+    {
+      navHeaderText: 'Tax and custom related crimes',
+      path: '#',
+    },
+  ]
+  // commercial sub list
+  const commercialSubNav = [
+    {
+      groupOne: [
+        {
+          navHeaderText: "Contract",
+          subNavList: [
+            {
+              navHeaderText: "Contract of special movables",
+              path: "#",
+            },
+            {
+              navHeaderText: "Sale or lease of buildings",
+              path: "#",
+            },
+            {
+              navHeaderText: "Construction",
+              path: "#",
+            },
+            {
+              navHeaderText: "Supply of goods and services",
+              path: "#",
+            },
+            {
+              navHeaderText: "Rental of machineries",
+              path: "#",
+            },
+            {
+              navHeaderText: "Loan",
+              path: "#",
+            },
+          ],
+        },
+        {
+          navHeaderText: "Intellectual property",
+          subNavList: [
+            {
+              navHeaderText: "Copyright",
+              path: "#",
+            },
+            {
+              navHeaderText: "Patent",
+              path: "#",
+            },
+            {
+              navHeaderText: "Trade mark",
+              path: "#",
+            },
+            {
+              navHeaderText: "Utilities",
+              path: "#",
+            },
+            {
+              navHeaderText: "Geographical indications",
+              path: "#",
+            },
+          ],
+        },
+        {
+          navHeaderText: "Financial sector",
+          subNavList: [
+            {
+              navHeaderText: "Banking",
+              path: "#",
+            },
+            {
+              navHeaderText: "Insurance",
+              path: "#",
+            },
+            {
+              navHeaderText: "Capital",
+              path: "#",
+            },
+          ],
+        },
+        {
+          navHeaderText: "Corporate",
+          subNavList: [
+            {
+              navHeaderText: "Incorporation",
+              path: "#",
+            },
+            {
+              navHeaderText: "Bankruptcy",
+              path: "#",
+            },
+            {
+              navHeaderText: "Merger",
+              path: "#",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      groupTwo: [
+        {
+          navHeaderText: "International trade and investment",
+          path: "#",
+        },
+        {
+          navHeaderText: "Tax",
+          path: "#",
+        },
+        {
+          navHeaderText: "Customs",
+          path: "#",
+        },
+        {
+          navHeaderText: "Real estate, property and conveyance",
+          path: "#",
+        },
+        {
+          navHeaderText: "Joint venture",
+          path: "#",
+        },
+        {
+          navHeaderText: "Torts",
+          path: "#",
+        },
+      ],
+    },
+  ];
+  // other services
+  const otherServicesSubNavList = [
+    {
+      navHeaderText: 'First service',
+      path: '#',
+    },
+    {
+      navHeaderText: 'Second service',
+      path: '#',
+    },
+    {
+      navHeaderText: 'Third service',
+      path: '#',
+    },
+  ]
+
   // nav drawer
   const mainNavDrawer = () => {
-    const mainNav = document.getElementById('main-nav-container') 
-    if(mainNav?.classList.contains('right-[-100vw]')){
-      mainNav?.classList.remove('right-[-100vw]')
-      mainNav?.classList.add('right-0')
-    }else {
-      mainNav?.classList.add('right-[-100vw]')
-      mainNav?.classList.remove('right-0')
+    const mainNav = document.getElementById("main-nav-container");
+    if (mainNav?.classList.contains("right-[-100vw]")) {
+      mainNav?.classList.remove("right-[-100vw]");
+      mainNav?.classList.add("right-0");
+    } else {
+      mainNav?.classList.add("right-[-100vw]");
+      mainNav?.classList.remove("right-0");
     }
-  }
+  };
+  
   return (
     <header className="shadow-headerBottomShadow fixed left-0 top-0 z-[1000] w-full bg-white text-black">
       <div className="px-[5%] flex items-center justify-between">
@@ -65,12 +293,19 @@ const Header = () => {
         <div className="flex-grow flex items-center justify-between relative">
           {/* nav container */}
           <div className="flex-grow flex items-center justify-center">
-            <div className="fixed w-full sm:w-[75%] md:w-[50%] h-[calc(100vh-9vh)] bg-gray-50 shadow-lg lg:shadow-none  right-[-100vw] transition-all ease-in-out duration-300 top-[9vh] lg:relative lg:bg-transparent lg:right-auto lg:top-auto lg:w-auto lg:h-auto" id="main-nav-container">
+            <div
+              className="fixed w-full sm:w-[75%] md:w-[50%] h-[calc(100vh-9vh)] bg-white shadow-lg lg:shadow-none  right-[-100vw] transition-all ease-in-out duration-300 top-[9vh] lg:relative lg:bg-transparent lg:right-auto lg:top-auto lg:w-auto lg:h-auto"
+              id="main-nav-container"
+            >
               <div className="w-full md:hidden flex items-center py-3 px-[5%]">
                 <div className="flex-grow flex items-center">
                   <div className="flex-grow flex items-center bg-white rounded-full py-1 border-2 border-black ">
-                    <FiSearch className="mx-2 text-lg"/>
-                    <input type="text" placeholder="search" className="w-full focus:ring-0 focus:outline-none border-none bg-transparent"/>
+                    <FiSearch className="mx-2 text-lg" />
+                    <input
+                      type="text"
+                      placeholder="search"
+                      className="w-full focus:ring-0 focus:outline-none border-none bg-transparent"
+                    />
                   </div>
                 </div>
               </div>
@@ -101,7 +336,182 @@ const Header = () => {
                       />
                     </NavLink>
                     {/* sub list pop up */}
-                    <div className={`absolute left-0 top-[100%] w-[350px] h-[200px] bg-red-300 transition-all ease-in-out duration-300 ${isNav?.navText === item.navText ? ' scale-100 opacity-100 ' : 'scale-0 opacity-0'}`}></div>
+                    <div
+                      className={`bg-white transition-all ease-in-out duration-300 ${
+                        isNav?.navText === item.navText
+                          ? " scale-100 opacity-100 "
+                          : "scale-0 opacity-0"
+                      } ${
+                        isNav?.navText === "Civil" ||
+                        isNav?.navText === "Commercial"
+                          ? "fixed left-0 top-[9.5vh] w-screen h-[calc(100vh-9.5vh)]"
+                          : "absolute left-0 top-[100%] w-[300px] whitespace-nowrap "
+                      }`}
+                    >
+                      {isNav?.navText === "Civil" ? (
+                        <div className="w-full h-full flex">
+                          <div className="flex-grow py-[3%] px-[20%] flex justify-between">
+                            <div>
+                              <h3 className="font-bold mb-1">
+                                {civilSubNav[0].groupOne[0].navHeaderText}
+                              </h3>
+                              <ul>
+                                {civilSubNav[0].groupOne[0].subNavList.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                            <div>
+                              <h3 className="font-bold mb-1">
+                                {civilSubNav[0].groupOne[1].navHeaderText}
+                              </h3>
+                              <ul>
+                                {civilSubNav[0].groupOne[1].subNavList.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                            <div>
+                              <ul>
+                                {civilSubNav[1].groupTwo.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      ) : isNav?.navText === "Criminal" ? (
+                        <div>
+                          <div className="p-[5%]">
+                            <ul>
+                              {
+                                criminalSubNav.map((item,index)=>(
+                                  <li key={index} className="my-1">
+                                    <NavLink className={'hover:underline'}>{item.navHeaderText}</NavLink>
+                                  </li>
+                                ))
+                              }
+                            </ul>
+                          </div>
+                        </div>
+                      ) : isNav?.navText === "Commercial" ? (
+                        <div className="w-full h-full flex">
+                          <div className="flex-grow py-[3%] px-[2%] flex justify-between">
+                            <div>
+                              <h3 className="font-bold mb-1">
+                                {commercialSubNav[0].groupOne[0].navHeaderText}
+                              </h3>
+                              <ul>
+                                {commercialSubNav[0].groupOne[0].subNavList.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                            <div>
+                              <h3 className="font-bold mb-1">
+                                {commercialSubNav[0].groupOne[1].navHeaderText}
+                              </h3>
+                              <ul>
+                                {commercialSubNav[0].groupOne[1].subNavList.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                            <div>
+                              <h3 className="font-bold mb-1">
+                                {commercialSubNav[0].groupOne[3].navHeaderText}
+                              </h3>
+                              <ul>
+                                {commercialSubNav[0].groupOne[3].subNavList.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                            <div>
+                              <h3 className="font-bold mb-1">
+                                {commercialSubNav[0].groupOne[2].navHeaderText}
+                              </h3>
+                              <ul>
+                                {commercialSubNav[0].groupOne[2].subNavList.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                            <div>
+                              <ul>
+                                {commercialSubNav[1].groupTwo.map(
+                                  (item, index) => (
+                                    <li key={index} className="my-1">
+                                      <NavLink className={"hover:underline"}>
+                                        {item.navHeaderText}
+                                      </NavLink>
+                                    </li>
+                                  )
+                                )}
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      ) : isNav?.navText === "Other Services" ? (
+                        <div>
+                          <div className="p-[5%]">
+                            <ul>
+                              {
+                                otherServicesSubNavList.map((item,index)=>(
+                                  <li key={index} className="my-1">
+                                    <NavLink className={'hover:underline'}>{item.navHeaderText}</NavLink>
+                                  </li>
+                                ))
+                              }
+                            </ul>
+                          </div>
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
                   </li>
                 ))}
               </ul>
@@ -127,10 +537,18 @@ const Header = () => {
               </div>
               {/* pop-up */}
               <div
-                className={`absolute left-0 top-[100%] bg-white w-max whitespace-nowrap text-[.975rem] shadow-lg p-5 overflow-hidden transition-all ease-in-out duration-300 ${
+                className={`fixed bottom-0 left-0 w-full md:absolute md:left-0 md:top-[100%] bg-white md:w-max md:h-max whitespace-nowrap text-[.975rem] shadow-lg p-5 overflow-hidden transition-all ease-in-out duration-300 ${
                   isPhone ? "scale-100 opacity-100" : "scale-0 opacity-0"
                 }`}
               >
+                {/* close btn */}
+                <div className="absolute top-1 right-1 md:hidden">
+                  <button onClick={()=>{
+                    setIsPhone(false)
+                  }}>
+                    <MdOutlineClose className="text-3xl"/>
+                  </button>
+                </div>
                 <div className="font-medium">
                   <h3>Talk to Makutta</h3>
                   <h3>(+2519) 239-96736</h3>
@@ -163,14 +581,17 @@ const Header = () => {
                   <span className="font-medium">sign in</span>
                 </div>
                 {/* icon for small screen */}
-                <RiUserShared2Fill className="md:hidden text-xl"/>
+                <RiUserShared2Fill className="md:hidden text-xl" />
               </NavLink>
             </div>
             {/* menu drawer */}
             <div className="lg:hidden">
-              <div className="mx-2 text-3xl cursor-pointer" onClick={()=>{
-                mainNavDrawer()
-              }}>
+              <div
+                className="mx-2 text-3xl cursor-pointer"
+                onClick={() => {
+                  mainNavDrawer();
+                }}
+              >
                 <MdMenu />
               </div>
             </div>
